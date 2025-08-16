5338de4fef18f428ca8708aafdb5e3e60409f276
ranjeethap <ranjeeth_ap@outlook.com>
2025-08-15 21:31:08 -0500

    Change greeting to new

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
-APP_VERSION = os.getenv("APP_VERSION", "Final Version")
+APP_VERSION = os.getenv("APP_VERSION", "Final Version-1")
 
 @app.get("/")
 def root():
