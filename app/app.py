ff0d3de91c26f5a7626c82880a48f98273171460
ranjeethap <ranjeeth_ap@outlook.com>
2025-08-16 16:35:34 -0500

    new versio update

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
-APP_VERSION = os.getenv("APP_VERSION", "Final Version-5")
+APP_VERSION = os.getenv("APP_VERSION", "Final Version-2")
 
 @app.get("/")
 def root():
