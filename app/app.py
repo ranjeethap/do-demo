80fa10b4587d82bafdc6ae96651f665688029f0a
ranjeethap <ranjeeth_ap@outlook.com>
2025-08-16 17:02:27 -0500

    version 3

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
-APP_VERSION = os.getenv("APP_VERSION", "Final Version-2")
+APP_VERSION = os.getenv("APP_VERSION", "Final Version-3")
 
 @app.get("/")
 def root():
